4e5ec9bcd41f0121da53df1e1051384da60d7287
nikhil-srikanth <95695256+nikhil-srikanth@users.noreply.github.com>
2023-01-30 15:39:19 +0530

    Create nsum.sh

---
+++ nsum.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Enter number : " 
+read n
+
+i=1
+sum=0
+echo "Digits : "
+while [ $i -le $n ]
+  do
+   echo "$i"
+   sum=$(( $sum + $i ))
+        i=$(( $i + 1 ))
+  done
+echo "Sum=$sum"
