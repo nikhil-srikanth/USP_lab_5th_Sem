c4ad41cd936c10bdd6c3a0e6475458653cc7a769
nikhil-srikanth <95695256+nikhil-srikanth@users.noreply.github.com>
2023-01-30 14:37:44 +0530

    Create grossal.sh

---
+++ grossal.sh
@@ -0,0 +1,15 @@
+echo "\nEnter name of Employee :"
+read name
+
+echo "\nEnter DA :"
+read da
+
+echo "\nEnter HRA:"
+read hra
+
+echo "\nEnter basic"
+read basic
+
+sal=$(( $da + $hra + $basic ))
+
+echo "\nGross Salary of $name is $sal"
